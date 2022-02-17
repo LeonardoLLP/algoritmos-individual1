141f321fe26c5a12434223b70ec0eaa7537046db
Leonardo Luque <64507345+LeonardoLLP@users.noreply.github.com>
2022-02-17 19:40:58 +0100

    Creado primer algoritmo ejercicio 9

---
+++ ejercicio9.py
@@ -0,0 +1,12 @@
+""" --- Algoritmo 1. Consulta. ---
+media(a: DECIMAL, b: DECIMAL, c: DECIMAL): DECIMAL
+
+poscondición
+    Resultado = (a + b + c) / 3
+
+fin media
+"""
+
+def media(a: float, b: float, c: float):
+    return (a + b + c) / 3
+
