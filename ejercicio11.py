d18f500bc99701eb6431a9e2a388ebc602257961
Leonardo Luque <64507345+LeonardoLLP@users.noreply.github.com>
2022-02-18 13:46:39 +0100

    Creada función para h_extra

--- ejercicio11.py
+++ ejercicio11.py
@@ -9,7 +9,7 @@ precondición
     s > 0
 
 variables
-    sh: DECIMAL <-- (s * 12) / (52 * 35)  # Salario por hora
+    sh: DECIMAL <-- (sm * 12) / (52 * 35)  # Salario por hora
 
 realización
     si h <= 35
@@ -18,7 +18,7 @@ realización
         si h <= 43
             Resultado <-- (sh * 1.25) * (h - 35)
         si no  # h > 43
-            Resultado <-- (sh * 1.50) * (h - 43) + (sh * 1.25)
+            Resultado <-- (sh * 1.50) * (h - 43) + (sh * 1.25) * 8
 
 poscondición
     si h <= 35
@@ -28,3 +28,15 @@ poscondición
 
 fin h_extra
 """
+
+def h_extra(h: int, sm: int):
+    sh = (sm * 12) / (52 * 35)
+
+    if h <= 35:
+        pass
+    else:
+        if h <= 43:
+            return (sh * 1.25) * (h - 35)
+        else:
+            return (sh * 1.50) * (h - 43) + (sh * 1.25) * 8
+
