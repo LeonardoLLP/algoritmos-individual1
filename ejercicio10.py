6bda226fa2043b5337307e91a3be1a6cecf06291
Leonardo Luque <64507345+LeonardoLLP@users.noreply.github.com>
2022-02-18 20:30:58 +0100

    Actualizado ejercicio 10 con precondiciones

--- ejercicio10.py
+++ ejercicio10.py
@@ -2,7 +2,7 @@
 def area_tri(l: DECIMAL, h: DECIMAL): DECIMAL
 
 precondición
-    l, a > 0
+    l, h > 0
 
 poscondición
     Resultado = l * a / 2
@@ -11,7 +11,10 @@ fin area_tri
 """
 
 def area_tri(l: float, h: float):
-    return l * h / 2
+    if l > 0 and h > 0:
+        return l * h / 2
+    else:
+        raise ValueError("Lenght and height must be greater than cero.")
 
 """ ¿Se puede utilizar este algoritmo para un triángulo rectángulo si se dan las medidas de sus dos lados perpendiculares?
 La respuesta es si. Siendo ambos lados perpendiculares, uno puede interpretarse como la altura del otro.
