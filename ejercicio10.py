18d13878086d4b50bda0a5fbe7ae135f8ade6d32
Leonardo Luque <64507345+LeonardoLLP@users.noreply.github.com>
2022-02-18 10:29:16 +0100

    Creación algoritmo ejercicio 10

---
+++ ejercicio10.py
@@ -0,0 +1,14 @@
+""" --- Algoritmo. Consulta. ---
+def area_tri(l: DECIMAL, h: DECIMAL): DECIMAL
+
+precondición
+    l, a > 0
+
+poscondición
+    Resultado = l * a / 2
+
+fin area_tri
+"""
+
+def area_tri(l: float, h: float):
+    return l * h / 2
