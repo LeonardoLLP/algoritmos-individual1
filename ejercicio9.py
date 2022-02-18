10c8f1ddadb1989980d4deb530a08b06b02281f6
Leonardo Luque <64507345+LeonardoLLP@users.noreply.github.com>
2022-02-18 10:22:31 +0100

    Creado segundo algoritmo ejercicio 9

--- ejercicio9.py
+++ ejercicio9.py
@@ -10,3 +10,18 @@ fin media
 def media(a: float, b: float, c: float):
     return (a + b + c) / 3
 
+
+""" --- Algoritmo 2. Consulta.
+media_ponderada(a: DECIMAL, ap: DECIMAL, b: DECIMAL, bp: DECIMAL, c: DECIMAL, cp: DECIMAL): DECIMAL
+
+precondición
+    ap, bp, cp >= 0
+
+poscondición
+    Resultado = (a*ap + b*bp + c*cp) / (ap+bp+cp)
+
+fin media_ponderada
+"""
+
+def media_ponderada(a: float, ap: float, b: float, bp: float, c: float, cp: float):
+    return (a*ap + b*bp + c*cp) / (ap+bp+cp)
