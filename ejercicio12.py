d33b51fc6541083fd727e959a933ac1ec7c87b10
Leonardo Luque <64507345+LeonardoLLP@users.noreply.github.com>
2022-02-18 20:10:00 +0100

    Actualizado valor cuenta y creado algoritmo 2

--- ejercicio12.py
+++ ejercicio12.py
@@ -1,4 +1,4 @@
-""" --- Clase 1. Comando. ---
+""" --- Clase 1.1 - Comando ---
 tipo CUENTA
 
 variables
@@ -22,6 +22,40 @@ fin CUENTA
 """
 
 class CUENTA:
+    def __init__(self, c: int):
+        self.c = c
+
+    def retirar(self, r):
+        if self.c > 0:
+            self.c -= r
+        else:
+            raise ValueError("Capital must be greater than cero.")
+
+
+""" --- Clase 1.2 - Comando ---
+tipo CUENTA_AVANZADA
+
+variables
+    c: ENTERO  # Capital en la cuenta
+
+algoritmo retirar (comando)
+
+    Entrada:
+        r: ENTERO  # Dinero a retirar
+
+    Salida:
+        c: ENTERO  # Dinero restante en la cuenta
+
+    precondición
+        c > 0
+
+    poscondición
+        c = antiguo(c) - r
+
+fin CUENTA
+"""
+
+class CUENTA_AVANZADA:
     def __init__(self, c: int):
         self.c = c
 
