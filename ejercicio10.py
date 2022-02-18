627829aba7b57647f9b21e249fec47d4214fd739
Leonardo Luque <64507345+LeonardoLLP@users.noreply.github.com>
2022-02-18 10:30:31 +0100

    Respondida pregunta ejercicio 10

--- ejercicio10.py
+++ ejercicio10.py
@@ -12,3 +12,7 @@ fin area_tri
 
 def area_tri(l: float, h: float):
     return l * h / 2
+
+""" ¿Se puede utilizar este algoritmo para un triángulo rectángulo si se dan las medidas de sus dos lados perpendiculares?
+La respuesta es si. Siendo ambos lados perpendiculares, uno puede interpretarse como la altura del otro.
+"""
